6a23c6df9cef2df747fb258db77b80cb2b2e1263
alenantony30 <alenantony30@gmail.com>
2024-08-02 13:06:37 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import exampleImage from './Logo.jpg';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UploadFile from './UploadFile';
-
+import { StepperProvider } from './StepperContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <div className='bankLogo'>
-        <img className='bankLogo' src={exampleImage} alt="Example" />
-      </div>
-      <Routes>
-        <Route path="/App" element={<App />} />
-        <Route path='/UploadFile' element={<UploadFile />} />
-      </Routes>
-    </BrowserRouter>
+    <StepperProvider>
+      <BrowserRouter>
+        <div className='bankLogo'>
+          <img className='bankLogo' src={exampleImage} alt="Example" />
+        </div>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/uploadFile" element={<UploadFile />} />
+        </Routes>
+      </BrowserRouter>
+    </StepperProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
